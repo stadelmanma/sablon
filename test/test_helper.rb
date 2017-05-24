5c1d51a724d1f5adc4209cb42b3312cb75cf0d58
Matthew Stadelman <mstadelman@mitre.org>
2017-05-24 07:46:10 -0400

    Remove dev-dependencies accidently comitted.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,20 +1,9 @@
-require 'simplecov'
-SimpleCov.start do
-  add_filter '/test/'
-end
-
 require "bundler/setup"
 require "minitest/autorun"
 require "minitest/mock"
 require "xmlsimple"
 require "json"
 require "pathname"
-
-
-if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
 
 $: << File.expand_path('../../lib', __FILE__)
 require "sablon"
