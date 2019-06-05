450aadda6ca3682e9cb5ea7e1da091d1d8c7191e
Matthew Stadelman <stadelmanma@gmail.com>
2019-06-05 11:12:59 -0400

    Pass a DOM object instead of a template object into Environment.new
    The Template being available doesn't make much sense but having the
    DOM is necessary.

--- lib/sablon/environment.rb
+++ lib/sablon/environment.rb
@@ -2,19 +2,14 @@ module Sablon
   # Combines the user supplied context and template into a single object
   # to manage data during template processing.
   class Environment
-    attr_reader :template
+    attr_reader :document
     attr_reader :context
     attr_reader :section_properties
 
     # returns a new environment with merged contexts
     def alter_context(context = {})
       new_context = @context.merge(context)
-      Environment.new(template, new_context)
-    end
-
-    # reader method for the DOM::Model instance stored on the template
-    def document
-      @template.document
+      Environment.new(document, new_context)
     end
 
     def section_properties=(properties)
@@ -23,8 +18,8 @@ module Sablon
 
     private
 
-    def initialize(template, context = {})
-      @template = template
+    def initialize(document, context = {})
+      @document = document
       @context = Context.transform_hash(context)
     end
   end
